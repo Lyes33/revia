8c0979fd5250764e7f48e3ae63e220f820eb14af
Blyes <bob.lyes@live.com>
2025-12-06 13:14:38 +0100

    add yml fil revia

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -8,7 +8,7 @@ test('Login successful', async ({ page }) => {
   await page.locator('[data-test="login-button"]').click()
 
   // Expect a title "to contain" a substring.
-  await expect(page.url()).toContain('/inventory.html');
+  await expect(page.url()).toContain('/invenqtory.html');
 });
 
 
