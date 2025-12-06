d16505640ef8bc9a8890bc5c54b6fcc4d663d92b
Blyes <bob.lyes@live.com>
2025-12-06 13:16:37 +0100

    test const

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Login successful', async ({ page }) => {
+  const user = 'standard_user'
   await page.goto('https://www.saucedemo.com/');
 
   await page.locator('[data-test="username"]').fill('standard_user')
